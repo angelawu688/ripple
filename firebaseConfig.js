9b673a65c58d13ed21222a874695939490a54e5e
ayaanrahim <ayaanarahim@gmail.com>
2024-11-05 22:29:42 -0800

    Initial commit

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAhRpIdHKZk_H9Jkp-In9Pc0YgB01pnnQE",
+    authDomain: "uw-marketplace-ebe58.firebaseapp.com",
+    projectId: "uw-marketplace-ebe58",
+    storageBucket: "uw-marketplace-ebe58.firebasestorage.app",
+    messagingSenderId: "869271955900",
+    appId: "1:869271955900:web:5e86631328a20eaf1f23b9",
+    measurementId: "G-D0QTKLFW34"
+  };
+
+  const app = initializeApp(firebaseConfig);
