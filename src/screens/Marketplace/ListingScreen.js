df4f8a799c4f9b83ae7da0cc709635e407f7a2fa
Angela Wu <angelawu097@gmail.com>
2024-11-19 20:23:36 -0800

    check if listing was saved by user

--- src/screens/Marketplace/ListingScreen.js
+++ src/screens/Marketplace/ListingScreen.js
@@ -1,7 +1,6 @@
 import { View, Text, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
-import { getFirestore, doc, getDoc, deleteDoc, setDoc } from "firebase/firestore";
-import {getAuth} from 'firebase/auth';
+import {getFirestore, doc, getDoc, getDocs, deleteDoc, setDoc, collection, query, where} from "firebase/firestore";
 import {useState, useEffect, useContext} from 'react';
 import {userContext} from "../../context/UserContext";
 
@@ -14,6 +13,7 @@ const ListingScreen = ({ route }) => {
     };
     const [listing, setListing] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    // when you onboard, need to know if listing is saved or not by the user
     const [isSaved, setIsSaved] = useState(true);
     const { listingID } = route.params;
     const { user } = useContext(userContext);
@@ -24,9 +24,27 @@ const ListingScreen = ({ route }) => {
           try {
             const docRef = doc(db, "listings", listingID);
             const docSnap = await getDoc(docRef);
-            
             if (docSnap.exists()) {
-              setListing({ id: docSnap.id, ...docSnap.data() });
+                setListing({ id: docSnap.id, ...docSnap.data() });
+                // check if listing is saved already
+                const savedPostsRef = collection(db, "savedPosts");
+                const queryPosts = query(savedPostsRef,
+                    where("user_id", "==", user.uid),
+                    where("listing_id", "==", listing.listing_id)
+                );
+                try {
+                    const savedSnapshot = await getDocs(queryPosts);
+                    if (!savedSnapshot.empty) {
+                        console.log("Listing is saved by the user.");
+                        setIsSaved(true);
+                    } else {
+                        console.log("Listing is not saved.");
+                        setIsSaved(false);
+                    }
+                } catch (error) {
+                    console.error("Error checking if listing is saved:", error);
+                }
+
             } else {
               console.log("No such document!");
             }
@@ -67,8 +85,9 @@ const ListingScreen = ({ route }) => {
             }
         } else if (listing) {
             // remove post from saved
-            const listingRef = doc(db, "savedPosts", listing.listing_id);
+            const listingRef = doc(db, "savedPosts", user.uid + listing.listing_id);
             await deleteDoc(listingRef);
+            console.log("deleted");
         }
         else {
             // should never hit this case
