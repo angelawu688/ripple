d359f077117de60da5ce36db44c90ebf79c628d2
Will Hunt <phunt22@uw.edu>
2024-11-28 13:46:46 -0800

    landing page animations

--- src/screens/Auth/LandingPage.js
+++ src/screens/Auth/LandingPage.js
@@ -1,15 +1,121 @@
-import { StyleSheet, Text, TouchableOpacity, View, useWindowDimensions } from "react-native"
+import { StyleSheet, Text, TouchableOpacity, View, useWindowDimensions, Animated } from "react-native"
 import Logo from "../../components/Logo";
 import { colors } from "../../colors";
+import { Wave, Wave2 } from "../shared/Wave";
+import { useEffect, useRef } from "react";
 
 
 const LandingPage = ({ navigation }) => {
     // TODO on component mount, we fade in the other pieces and have them be animated and stuff
+
+    const randomize = (min, max) => Math.random() * (max - min) + min;
+
+    // create 8 animations refs (x, y)
+    const waveAnimations = Array(8).fill(0).map(() => ({
+        x: useRef(new Animated.Value(0)).current,
+        y: useRef(new Animated.Value(0)).current,
+    }));
+
+    // define and start animations based for each of the refs
+    useEffect(() => {
+        waveAnimations.forEach(({ x, y }) => {
+            Animated.loop(
+                Animated.parallel([
+                    Animated.sequence([
+                        Animated.timing(x, {
+                            toValue: randomize(-120, 80),
+                            duration: randomize(3000, 8000),
+                            useNativeDriver: true,
+                        }),
+                        Animated.timing(x, {
+                            toValue: 0,
+                            duration: randomize(3000, 8000),
+                            useNativeDriver: true,
+                        }),
+                    ]),
+                    Animated.sequence([
+                        Animated.timing(y, {
+                            toValue: randomize(-30, 30),
+                            duration: randomize(3000, 8000),
+                            useNativeDriver: true,
+                        }),
+                        Animated.timing(y, {
+                            toValue: 0,
+                            duration: randomize(3000, 8000),
+                            useNativeDriver: true,
+                        }),
+                    ]),
+                ])
+            ).start();
+        });
+    }, [waveAnimations]);
+
     return (
         <View style={styles.container}>
 
-            <View style={{ marginTop: '-35%' }}>
-                <Logo fontSize={65} />
+            {/* Top waves */}
+            <View style={styles.topWaves}>
+                {waveAnimations.slice(0, 4).map((animation, index) => (
+                    <Wave
+                        key={`top-wave-${index}`}
+                        width={randomize(500, 600)}
+                        color={colors.neonBlue}
+                        animatedStyle={{
+                            transform: [
+                                { translateX: animation.x },
+                                { translateY: animation.y },
+                            ],
+                        }}
+                    />
+                ))}
+                {/* <Text>
+                    Top
+                </Text> */}
+            </View>
+
+
+            {/* Logo */}
+            <View style={styles.logoContainer}>
+                {/* <Logo fontSize={65} /> */}
+                <Text style={{ fontFamily: 'Syne', fontWeight: '700', fontSize: 65, color: '#51A2B0' }}>
+                    Ripple
+                </Text>
+            </View>
+
+            {/* wave 2––being weird in the bottom waves view so if we have issues this is why */}
+            <View style={{ position: 'absolute', top: '20%', left: -50 }}>
+                <Wave2 width={520} color={colors.loginBlue}
+                    style={{ position: 'absolute', top: '-20%' }}
+                    animatedStyle={{
+                        transform: [
+                            // 7 is the last one
+                            { translateX: waveAnimations[7].x },
+                            { translateY: waveAnimations[7].y },
+                        ],
+                    }} />
+            </View>
+
+
+            {/* Bottom waves */}
+            <View style={styles.bottomWaves}>
+
+                {/* the last 3  */}
+                {waveAnimations.slice(4).map((animation, index) => (
+                    <Wave
+                        key={`bottom-wave-${index}`}
+                        width={randomize(500, 600)}
+                        color={colors.neonBlue}
+                        animatedStyle={{
+                            transform: [
+                                { translateX: animation.x },
+                                { translateY: animation.y },
+                            ],
+                        }}
+                    />
+                ))}
+                {/* <Text>
+                    Bottom
+                </Text> */}
             </View>
 
 
@@ -26,26 +132,6 @@ const LandingPage = ({ navigation }) => {
                         Login
                     </Text>
                 </TouchableOpacity>
-
-                {/* STRICTLY FOR TESTING */}
-                {/* <TouchableOpacity
-                    onPress={() => {
-                        //  password, major, concentration, gradYear, name } = route.params
-                        navigation.navigate('InfoOnboarding', {
-                            email: 'phunt22@useWindowDimensions.edu',
-                            password: 'password',
-                            major: 'CS',
-                            concentration: 'cs',
-                            gradYear: 2040,
-                            name: 'Will'
-                        })
-                    }}
-
-                >
-                    <Text>
-                        info ob
-                    </Text>
-                </TouchableOpacity> */}
             </View>
         </View>
     )
@@ -66,7 +152,7 @@ const styles = StyleSheet.create({
     },
     button: {
         width: '90%',
-        height: 40,
+        height: 45,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
@@ -78,12 +164,33 @@ const styles = StyleSheet.create({
         fontWeight: '600'
     },
     buttonContainer: {
-        height: 90,
+        height: 110,
         flexDirection: 'column',
         justifyContent: 'space-between',
         position: 'absolute',
         bottom: 75,
         width: '100%',
         alignItems: 'center'
-    }
+    },
+    topWaves: {
+        position: 'absolute',
+        top: 0,
+        width: '100%',
+        alignItems: 'center',
+        zIndex: 2, // Ensure top waves are above bottom waves,
+    },
+    bottomWaves: {
+        position: 'absolute',
+        top: '40%',
+        width: '100%',
+        alignItems: 'center',
+        zIndex: 1, // Lower zIndex than top waves
+    },
+    logoContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        top: '35%'
+        // marginVertical: 40, // Space between waves and logo
+    },
 })
