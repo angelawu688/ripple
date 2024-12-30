9ed751152f10f6fd2cde7eced2013efe17849a5b
William Hunt <phunt22@uw.edu>
2024-12-29 17:04:37 -0800

    typo

--- src/hooks/useAuthNavigation.js
+++ src/hooks/useAuthNavigation.js
@@ -3,7 +3,7 @@ import { userContext } from "../context/UserContext"
 
 export const useAuthNavigation = () => {
     const { user, userData, isLoading } = useContext(userContext)
-    const isAuthenticated = user && userData.emailVerified
+    const isAuthenticated = user && user.emailVerified
 
     return {
         isLoading,
