97cb3b27db38d5a0a9611ac542b77fd76d39a45f
William Hunt <phunt22@uw.edu>
2025-01-15 11:11:31 -0800

    updated font loading

--- src/config/fonts.js
+++ src/config/fonts.js
@@ -1,13 +1,60 @@
-import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
-import { Syne_700Bold, Syne_400Regular } from '@expo-google-fonts/syne';
-import { Inter_400Regular } from '@expo-google-fonts/inter'
-import { Rubik_400Regular, Rubik_700Bold } from '@expo-google-fonts/rubik'
+// import { Roboto_400Regular } from '@expo-google-fonts/roboto';
+// import { Syne_700Bold, Syne_400Regular } from '@expo-google-fonts/syne';
+// import { Inter_400Regular } from '@expo-google-fonts/inter'
+// import { Rubik_400Regular, Rubik_700Bold } from '@expo-google-fonts/rubik'
+import {
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import {
+    Syne_400Regular,
+    Syne_500Medium,
+    Syne_600SemiBold,
+    Syne_700Bold,
+    Syne_800ExtraBold,
+} from '@expo-google-fonts/syne';
+import {
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+} from '@expo-google-fonts/inter';
+import {
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_600SemiBold,
+    Rubik_700Bold,
+    Rubik_800ExtraBold,
+} from '@expo-google-fonts/rubik';
 
+
+// export const FONTS = {
+//     Roboto_400Regular,
+//     Syne_700Bold,
+//     Syne_400Regular,
+//     Inter_400Regular,
+//     Rubik_400Regular, Rubik_700Bold
+// }
 
 export const FONTS = {
     Roboto_400Regular,
-    Syne_700Bold,
+    Roboto_500Medium,
+    Roboto_700Bold,
     Syne_400Regular,
+    Syne_500Medium,
+    Syne_600SemiBold,
+    Syne_700Bold,
+    Syne_800ExtraBold,
     Inter_400Regular,
-    Rubik_400Regular, Rubik_700Bold
-}
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_600SemiBold,
+    Rubik_700Bold,
+    Rubik_800ExtraBold,
+};
